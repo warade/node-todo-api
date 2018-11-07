f3d419deb479c38bc2b8132e9ee6fc6b6a567aa7
warade <warade.anshul@gmail.com>
2018-11-07 14:38:43 +0530

    test POST /todos routes

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.post('/todos', (req, res) => {
 	todo.save().then((doc) => {
 		res.send(doc);
 	}, (e) => {
-		res.send(e);
+		res.status(400).send(e);
 	});
 });
 
@@ -24,6 +24,7 @@ app.listen(3000, () => {
 	console.log(`Running on port 3000.`);
 });
 
+module.exports = {app};
 
 
 

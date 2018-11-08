7f36f52653e1ace8e7a9d1d473bd5663b7733dad
warade <warade.anshul@gmail.com>
2018-11-08 23:28:52 +0530

    Add PATCH /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -9,4 +9,5 @@ module.exports = {mongoose};
 // heroku config:set MONGOLAB_URI="mongodb://faraday_anshul:chandra123@ds151463.mlab.com:51463/node-todo-api"
 
 
+
 // mongodb://faraday_anshul:chandra123@ds151463.mlab.com:51463/node-todo-api
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -51,6 +52,22 @@ app.delete('/todos/:id', (req, res) => {
 		res.status(400).send(e);
 	});
 });
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+	if(!ObjectID.isValid(id)) return res.status(404).send();
+	if(!_.isBoolean(body.completed)) res.status(400).send({error: 'Boolean value required!'});
+	if(_.isBoolean(body.completed) && body.completed) 
+		body.completedAt = new Date().getTime();
+	else body.completed = false, body.completedAt = null;
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if(!todo) return res.status(404).send();
+		res.send({todo});
+	}).catch((e) => res.status(400).send(e));
+});
+
 
 app.listen(port, () => {
 	console.log(`Running on port ${port}.`);
